21aa22b92d492b1ec83e74d78cc55654557b957d
elliot-tunstall <elliot.tunstall@gmail.com>
2025-07-30 15:45:00 +0100

    built location permisions from the user

--- src/MapScreen.tsx
+++ src/MapScreen.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import { Button, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Button, View, Alert, Platform } from 'react-native';
 import MapView from 'react-native-maps';
+import { check, request, RESULTS, PERMISSIONS } from 'react-native-permissions';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+// PERSMISSIONS
+const ANDROID_PERMISSION = PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION;
+const IOS_PERMISSION = PERMISSIONS.IOS.LOCATION_WHEN_IN_USE;
+
 const MapScreen = () => {
   const navigation = useNavigation<NativeStackNavigationProp<any>>();
 
-  const reqeustPermisions = () => {
-    // TODO: Implement permission request functionality
+  const requestPermissions = async () => {
+    const permission = (Platform.OS === 'android') ? ANDROID_PERMISSION : IOS_PERMISSION;
+
+    let status = await check(permission);
+    if (status !== RESULTS.GRANTED) {
+      status = await request(permission);
+    }
+
+    console.log(status);
+    if (status !== RESULTS.GRANTED) {
+      Alert.alert(
+        'Permission needed',
+        'We need access to your location to track your activity.',
+      );
+    }
+
+    return status === RESULTS.GRANTED;
   };
 
-  const startTracking = () => {
+  useEffect(() => {
+    // request permissions
+    const checkPermissions = async () => {
+      const hasPermission = await requestPermissions();
+      // if no permission, return
+      if (!hasPermission) {
+        console.warn("Permissions not granted")
+        return;
+      }
+    };
+    
+    checkPermissions();
+  }, [])
+
+  const startTracking = async () => {
     // TODO: Implement tracking functionality
+    if (!await requestPermissions()) {
+      return;
+    }
   };
 
   const saveWorkout = () => {
